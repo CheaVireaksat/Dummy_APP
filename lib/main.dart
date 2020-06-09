9f23e6c241788f81b9b811beedc9068a4c99c55c
CheaVireaksat <cheavireaksat@gmail.com>
2020-06-09 15:22:32 +0700

    Updated_Widgets

--- lib/main.dart
+++ lib/main.dart
@@ -1,31 +1,123 @@
 import 'package:flutter/material.dart';
+import 'package:persional_expanse_app/transaction.dart';
+import 'package:intl/intl.dart';
 
 void main() {
   runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
+  final List<Transaction> transactions = [
+    Transaction(
+      id: 'A1',
+      title: 'New Shoes',
+      amount: 69.99,
+      date: DateTime.now(),
+    ),
+    Transaction(
+      id: 'A2',
+      title: 'New Shock',
+      amount: 39.99,
+      date: DateTime.now(),
+    ),
+    Transaction(
+      id: 'A3',
+      title: 'Weekly Groceries',
+      amount: 989.99,
+      date: DateTime.now(),
+    ),
+  ];
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
         appBar: AppBar(
-          title: Icon(Icons.home),
-          backgroundColor: Colors.purple,
+          title: Text('My Transaction'),
+          backgroundColor: Colors.teal[400],
         ),
         body: Column(
           // Parent of the card
-          mainAxisAlignment: MainAxisAlignment.spaceAround,
+          //mainAxisAlignment: MainAxisAlignment.spaceAround,
           crossAxisAlignment: CrossAxisAlignment.stretch,
           children: <Widget>[
-            Card(
-              color: Colors.lightBlue,
-              child: Container(width: double.infinity, child: Text('CHART!')),
-              elevation: 5,
+            Container(
+              child: Card(
+                //color: Colors.lightBlue,
+                child: Container(
+                  width: double.infinity,
+                  child: Text(
+                    'Input here!',
+                    style: TextStyle(fontWeight: FontWeight.bold, fontSize: 15, color: Colors.black87),
+                  ),
+                ),
+                elevation: 3,
+              ),
             ),
             Card(
-              color: Colors.pinkAccent,
-              child: Text('LIST OF TX!'),
+              elevation: 3,
+              child: Container(
+                padding: EdgeInsets.all(10),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.end,
+                  children: <Widget>[
+                    //TextField widget is use for recive user input
+                    TextField(
+                      decoration: InputDecoration(labelText: 'Title'),
+                    ),
+                    TextField(
+                      decoration: InputDecoration(labelText: 'Amount'),
+                    ),
+                    FlatButton(
+                      child: Text('Add Transaction'),
+                      textColor: Colors.deepPurple,
+                      onPressed: () {},
+                    ),
+                  ],
+                ),
+              ),
+            ),
+            Column(
+              children: transactions.map((tx) {
+                return Card(
+                  child: Row(
+                    children: <Widget>[
+                      Container(
+                        margin:
+                            EdgeInsets.symmetric(vertical: 10, horizontal: 15),
+                        decoration: BoxDecoration(
+                          border: Border.all(
+                            color: Colors.purpleAccent,
+                            width: 2,
+                          ),
+                        ),
+                        padding: EdgeInsets.all(10),
+                        child: Text(
+                          '\$ ${tx.amount}', // dart will covert it to string by using $ sign (string interpolation)
+                          style: TextStyle(
+                            fontWeight: FontWeight.bold,
+                            fontSize: 20,
+                            color: Colors.purpleAccent,
+                            // on the Colors class purple is a static properties
+                          ),
+                        ),
+                      ),
+                      Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: <Widget>[
+                            Text(
+                              tx.title,
+                              style: TextStyle(
+                                  fontSize: 15, fontWeight: FontWeight.bold),
+                            ),
+                            Text(
+                              DateFormat.yMMMMEEEEd().format(tx.date),
+                              style: TextStyle(color: Colors.blueGrey),
+                            )
+                          ])
+                    ],
+                  ),
+                );
+              }).toList(),
             ),
           ],
         ),
